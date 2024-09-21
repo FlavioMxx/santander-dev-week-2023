a4cb15aeec65fee0b77304bcea93e1676ce85702
Flavio Milani <frmilanij@gmail.com>
2024-09-21 11:03:50 -0300

    bugfix cors

--- src/main/java/br/edu/flaviomxx/dio/Application.java
+++ src/main/java/br/edu/flaviomxx/dio/Application.java
@@ -1,8 +1,11 @@
 package br.edu.flaviomxx.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Url padrão")})
 @SpringBootApplication
 public class Application {
 
